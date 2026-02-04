fa9881ddcb4ad7fd20add6fd56c06f1784a8a540
Nathala <sunilnaathaala@gmail.com>
2026-02-04 22:02:21 +0000

    added something

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hello Ramesh, I am fine thanks for asking, How are you?"
+echo "Suresh:: I am doing fine, I am learning devops, how about you?"
+echo "Suresh:: thats good. enjoy"
