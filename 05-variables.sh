1748ca98a23523eaac2bf25c8050f2dce6e61840
Nathala <sunilnaathaala@gmail.com>
2026-02-04 22:24:55 +0000

    added something

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+read USER_NAME
+echo "User Name is $USER_NAME"
+
+echo "Please enter your PASSWORD::"
+read -s PASSWORD
+echo "Password is $PASSWORD"
