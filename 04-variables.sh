f2ee2696ef6223d1c0223f4be01b046e1477527b
Nathala <sunilnaathaala@gmail.com>
2026-02-04 22:17:52 +0000

    added something

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 # no space between value and =
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine thanks for asking, How are you?"
+echo "$PERSON1:: I am doing fine, I am learning devops, how about you?"
+echo "$PERSON2:: thats good. enjoy"
