48b45e8b8ba708252bac2371f30bc7d34241afb8
Nathala <sunilnaathaala@gmail.com>
2026-02-05 18:23:27 +0000

    added something

---
+++ 11-functions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then
+    echo "$2 ......FAILURE"
+    exit 1
+else
+    echo "$2 ......SUCCESS"
+fi
+
+}
+
+echo "installing Nginx"
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+echo "installing MYSQL"
+dnf install mysql -y
+VALIDATE $? "Installing MySQL"
+
+
+echo "installing Nodejs"
+dnf install nodejs -y
+VALIDATE $? "Installing NodeJS"
