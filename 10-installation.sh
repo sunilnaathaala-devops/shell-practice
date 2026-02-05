9f0d8e32056c32d6364daf3db4381cac2a088875
Nathala <sunilnaathaala@gmail.com>
2026-02-05 17:56:17 +0000

    added something

--- 10-installation.sh
+++ 10-installation.sh
@@ -16,3 +16,23 @@ if [ $? -ne 0 ]; then
 else
     echo "Installing Nginx ......SUCCESS"
 fi
+
+echo "installing MYSQL"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing MYSQL ......FAILURE"
+    exit 1
+else
+    echo "Installing MYSQL ......SUCCESS"
+fi
+
+echo "installing Nodejs"
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nodejs ......FAILURE"
+    exit 1
+else
+    echo "Installing nodejs ......SUCCESS"
+fi
